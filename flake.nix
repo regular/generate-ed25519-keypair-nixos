62561ffa79b6182c4832a9dbabe7cab842e63d1f
regular <jan@lagomorph.de>
2025-08-26 10:25:54 +0200

    Initial

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Generate persistent and non-persistent ED25519 keypairs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.demo = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        self.nixosModules.default
+        {
+            services.generate-ED25519.enable = true;
+        }
+      ];
+    };
+    nixosModules.default = (import ./service.nix) self;
+    packages.${system}.genkeypair = pkgs.writeShellScriptBin "genkeypair" ''
+      export PATH=${pkgs.lib.makeBinPath (with pkgs; [
+        openssl
+        gawk
+      ])}
+      pem="$(openssl genpkey -algorithm ED25519 -out - 2>/dev/null)"
+      dump="$(printf "%s" "$pem" | openssl pkey -text -noout)"
+
+      privhex=$(
+        printf "%s\n" "$dump" |
+        awk '/^priv:/{m=1;next} /^pub:/{m=0} m{gsub(/[^0-9a-f]/,""); printf "%s",$0}'
+      )
+
+      pubhex=$(
+        printf "%s\n" "$dump" |
+        awk '/^pub:/{m=1;next} m{gsub(/[^0-9a-f]/,""); printf "%s",$0}'
+      )
+
+      # sanity: 32 bytes each (64 hex chars)
+      [ ''${#privhex} -eq 64 ] && [ ''${#pubhex} -eq 64 ] || { echo "bad key lengths" >&2; exit 1; }
+
+      printf "%s\n%s\n" "$privhex" "$pubhex"
+    '';
+  };
+}
---
+++ service.nix
@@ -0,0 +1,32 @@
+
+self: { config, lib, pkgs, ... }:
+let
+  cfg = config.services.generate-ED25519;
+in {
+  options.services.generate-ED25519 = with lib; {
+    enable = mkEnableOption "keypair generator service";
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.generate-ED25519 = {
+      description = "Generate ED25519 keypairs service";
+      #ConditionPathExists=!/etc/credstore.encrypted/machine-master-ed25519.cred
+
+      serviceConfig = {
+        Type = "oneshot";
+        After = ["local-fs.target"];
+        UMask = "0077";
+        ExecStart = ''
+          cat <<<'EOF' | ${pkgs.stdenv.shell} -euo pipefail
+            ${self.packages.${pkgs.system}.genkeypair}/bin/genkeypair | \
+            systemd-creds encrypt \
+              --name=session-ed25519 - \
+              /etc/encrypted/session-ed25519
+          EOF
+        '';
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
